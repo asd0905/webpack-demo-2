20f3113724cab5c3f957c0fe9cb58caf63c3db19
asd0905 <asd0905@naver.com>
2021-01-11 16:22:57 +0900

    번들 업데이트

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import _ from 'lodash';
 
 function component() {
     console.log(333);
+
+    /*var myFirstPromise = new Promise(function(resolve, reject) {
+        // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+        // In this example, we use setTimeout(...) to simulate async code.
+        // In reality, you will probably be using something like XHR or an HTML5 API.
+        setTimeout(function(){
+            resolve("Success!"); // Yay! Everything went well!
+        }, 250);
+    });*/
+
+
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work (before)
