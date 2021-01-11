45eb5aac68500cabd5b16d7f13e8e3cf5fd870a0
asd0905 <asd0905@naver.com>
2021-01-11 16:11:44 +0900

    ts파일 생성

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/*import * as _ from 'lodash';
+
+function component() {
+    console.log(444);
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());*/
+
+export class TestClass {
+    constructor() {
+    }
+
+    test() {
+        console.log('test1');
+    }
+}
+
+const testC = new TestClass();
+testC.test();
